5b9792937cef3396ce9ce4b2f9ba0c4eb7d64b30
CHRISTIANGUERRERO1022 <guerrerochristian711@gmail.com>
2024-10-10 17:36:33 -0600

    menu index
    menu index

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,15 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    /*return view('welcome');*/
+    return "Bienvenido a la pagina";
 });
+ /*Route:: get('cursos', function () {
+    return "Bienvenido a la pagina de cursos";
+});
+Route:: get('create', function () {
+    return "podras crear un curso:";
+});
+Route:: get('cursos/{curso}', function ($curso) {
+    return "bienvenido al curso: $curso";
+});*/
