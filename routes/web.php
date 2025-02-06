2c90c40d44d5593fe5ab33f93ba30a7ac3f89695
christianguerrero <guerrerochristian711@gmail.com>
2025-02-06 10:56:01 -0600

    busqueda

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\RegistroController;
 use App\Http\Controllers\AreasController;
 use App\Http\Controllers\RolesPermisoController;
 use App\Http\Controllers\PreparadoController;
+use App\Http\Controllers\BusquedaController;
 
 use App\Http\Controllers\DashboardControlle;
 use GuzzleHttp\Promise\Coroutine;
