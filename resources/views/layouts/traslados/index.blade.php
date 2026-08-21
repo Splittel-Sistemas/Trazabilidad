933d7ae242be82e0ad7ca51ec306865ab1867d31
Parkz <yosimar.dev@gmail.com>
2026-08-21 13:49:15 -0600

    Fix: tabla en vista crear traslado ahora con datatble, ajuste de idioma en datatables y cambio de color en vistas de traslado

--- resources/views/layouts/traslados/index.blade.php
+++ resources/views/layouts/traslados/index.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Traslados de Almacenes')
 
 @section('content')
-<div class="container mt-5">
+<div class="container mt-5 bg-white p-5">
     <h1 class="mb-4 text-center">Traslados de Almacenes</h1>
 
     <div class="row">
@@ -65,8 +65,29 @@
 
 @section('scripts')
 <script>
-    window.addEventListener('DOMContentLoaded', function () {
-        $('.table').DataTable();
+    window.addEventListener('DOMContentLoaded', function() {
+        $('.table').DataTable({
+            language: {
+                "decimal": "",
+                "emptyTable": "No hay información",
+                "info": "Mostrando _START_ a _END_ de _TOTAL_ Entradas",
+                "infoEmpty": "Mostrando 0 to 0 of 0 Entradas",
+                "infoFiltered": "(Filtrado de _MAX_ total entradas)",
+                "infoPostFix": "",
+                "thousands": ",",
+                "lengthMenu": "Mostrar _MENU_ Entradas",
+                "loadingRecords": "Cargando...",
+                "processing": "Procesando...",
+                "search": "Buscar:",
+                "zeroRecords": "Sin resultados encontrados",
+                "paginate": {
+                    "first": "Primero",
+                    "last": "Ultimo",
+                    "next": "Siguiente",
+                    "previous": "Anterior"
+                }
+            },
+        });
     });
 </script>
 @endsection
